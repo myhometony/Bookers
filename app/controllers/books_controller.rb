7fe375999f927ce0eaf374538d9e3cdc33624c87
EC2 Default User <ec2-user@ip-172-31-83-76.ec2.internal>
2022-04-12 09:58:54 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to '/books', notice: 'Successfully destroyed.'
   end
 
   def update
