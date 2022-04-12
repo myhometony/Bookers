a1bdc546326b0dbf1807209582d872317d3fb359
EC2 Default User <ec2-user@ip-172-31-83-76.ec2.internal>
2022-04-12 05:15:02 +0000

    [Add]vali.cre,up

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,20 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to book_path(book.id), notice: 'Thank you! Successfully created!'
+    @book = Book.new(book_params)#bookから@booknに変更
+    if @book.save#bookから@booknに変更
+    redirect_to book_path(@book.id), notice: 'Thank you! Successfully created!'
+   #↑bookから@booknに変更
     else
-      redirect_to '/books'
-      #render :books
+      @books = Book.all#インスタンス変数追加
+      render :index #booksをindexに変更
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    #インスタンス変数@bookを追加
 
   end
 
@@ -33,11 +36,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Thank you! Successfully updated!'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Thank you! Successfully updated!'
     else
-      render :books
+      @book = Book.find(params[:id])
+      render :edit
     end
   end
 
